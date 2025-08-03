0255348ada3cf182ced0256663fa554f8d5b04d6
Navid Habibi <navid.habibi6650@gmail.com>
2025-08-03 08:45:03 +0330

    get serial command for change method

--- Zlab_Fanap_SRF05/Zlab_Fanap_SRF05.ino
+++ Zlab_Fanap_SRF05/Zlab_Fanap_SRF05.ino
@@ -3,6 +3,21 @@ void setup() {
 
 }
 
+void checkSerialForUnit() {
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    input.toUpperCase();
+    if (input == "CM" || input == "MM" || input == "INCH") {
+      defaultUnit = input;
+      Serial.print("Unit changed to: ");
+      Serial.println(defaultUnit);
+    } else {
+      Serial.println("Invalid unit! Use CM, MM, or INCH.");
+    }
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
 
